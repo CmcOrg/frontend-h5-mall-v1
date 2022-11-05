494cf76ba55b958f3ebcb586b759b647b7161d6a
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-05 12:41:23 +0800

    【i】【CmcOrg】

--- src/page/takeaway/Welcome/Welcome.tsx
+++ src/page/takeaway/Welcome/Welcome.tsx
@@ -1,11 +1,11 @@
 import {PageHeader} from "antd";
 
-// 商城平台-欢迎页
+// 外卖平台-欢迎页
 export default function () {
 
     return (
         <PageHeader
-            title={"商城平台"}
+            title={"外卖平台"}
         />
     )
 
