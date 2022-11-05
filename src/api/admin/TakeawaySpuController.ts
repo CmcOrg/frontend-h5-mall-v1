5cf69eff0392384e5111930326b32b7093437b37
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-05 19:02:41 +0800

    【i】【CmcOrg】

--- src/api/admin/TakeawaySpuController.ts
+++ src/api/admin/TakeawaySpuController.ts
@@ -16,7 +16,9 @@ export interface NotNullId {
     id: number // 主键id {"min":1}
 }
 
-export interface TakeawaySpuDO {
+export interface TakeawaySpuInfoByIdVO {
+    categoryIdSet?: number[] // 商品分类主键 idSet
+    specJsonListStrSet?: string[] // 规格 json对象集合字符串 set
     name?: string // SPU名称
     scene?: 1 | 2 // 场景：1 堂食 2 外卖
     mustFlag?: boolean // 是否必选，即：不选无法下单
@@ -34,7 +36,7 @@ export interface TakeawaySpuDO {
 
 // 通过主键id，查看详情
 export function TakeawaySpuInfoById(form: NotNullId, config?: AxiosRequestConfig) {
-    return $http.myProPost<TakeawaySpuDO>('/takeaway/spu/infoById', form, config)
+    return $http.myProPost<TakeawaySpuInfoByIdVO>('/takeaway/spu/infoById', form, config)
 }
 
 export interface TakeawaySpuInsertOrUpdateDTO {
@@ -44,6 +46,8 @@ export interface TakeawaySpuInsertOrUpdateDTO {
     orderNo?: number // 排序号（值越大越前面，默认为 0）
     enableFlag?: boolean // 是否启用
     remark?: string // 备注
+    categoryIdSet?: number[] // 商品分类主键 idSet
+    specJsonListStrSet: string[] // 规格 json对象集合字符串 set
     id?: number // 主键id {"min":1}
 }
 
@@ -63,6 +67,22 @@ export interface TakeawaySpuPageDTO {
     pageSize?: number // 每页显示条数
     order?: MyOrderDTO // 排序字段
     sort?: Record<string, SortOrder> // 排序字段（只在前端使用，实际传值：order）
+}
+
+export interface TakeawaySpuDO {
+    name?: string // SPU名称
+    scene?: 1 | 2 // 场景：1 堂食 2 外卖
+    mustFlag?: boolean // 是否必选，即：不选无法下单
+    orderNo?: number // 排序号（值越大越前面，默认为 0）
+    id?: number // 主键id
+    createId?: number // 创建人id
+    createTime?: string // 创建时间
+    updateId?: number // 修改人id
+    updateTime?: string // 修改时间
+    version?: number // 乐观锁
+    enableFlag?: boolean // 是否启用
+    delFlag?: boolean // 是否逻辑删除
+    remark?: string // 备注
 }
 
 // 分页排序查询
