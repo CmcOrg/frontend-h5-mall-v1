d5c9147fb50cfb60f357e55e6c400084dc37ae42
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-20 20:20:09 +0800

    【i】【CmcOrg】

--- src/api/sign/SignWxController.ts
+++ src/api/sign/SignWxController.ts
@@ -1,6 +1,15 @@
 import $http from "@/util/HttpUtil";
 import {AxiosRequestConfig} from "axios";
 
+export interface SignInCodeDTO {
+    code: string // 微信 code
+}
+
+// 微信 code登录
+export function SignWxSignInCode(form: SignInCodeDTO, config?: AxiosRequestConfig) {
+    return $http.myPost<string>('/sign/wx/sign/in/code', form, config)
+}
+
 export interface SignInPhoneCodeDTO {
     phoneCode: string // 手机号码 code
 }
